db6a784625e12477de1fd7c2fcf87315297c8357
Jennifer Da Silva <jenniferdsbaumgart@gmail.com>
2023-03-07 16:52:04 -0300

    projectCalculadora

---
+++ calculadora.js
@@ -0,0 +1,30 @@
+const campo1 = document.querySelector("#campo1");
+const campo2 = document.querySelector("#campo2");
+const seletor = document.querySelector("#operacao");
+const botao = document.querySelector("#igual");
+let resposta = document.querySelector("#resposta");
+
+botao.addEventListener("click", calcular);
+
+function calcular(){
+  if(campo1.value==='' || campo2.value===''){
+    resposta.classList.add("problema");
+    resposta.innerHTML="Campo vazio!";
+    setTimeout(() => {
+      resposta.classList.remove("problema");
+      resposta.innerHTML='';
+    }, 3000);
+  } else{
+      const valor1 = parseInt(campo1.value);
+      const valor2 = parseInt(campo2.value);
+      const operacao = seletor.value;
+      if(operacao==="Somar")
+        resposta.innerHTML = valor1 + valor2;
+      if(operacao==="Subtrair")
+        resposta.innerHTML = valor1 - valor2;
+      if(operacao==="Multiplicar")
+        resposta.innerHTML = valor1 * valor2;
+      if(operacao==="Dividir")
+        resposta.innerHTML = valor1 / valor2;
+  }
+}
